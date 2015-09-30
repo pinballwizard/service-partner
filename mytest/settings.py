9bc71d68b63bdd3c50c357e894e154f39ebd6623
craby-c <securicula@gmail.com>
2015-09-30 15:23:33 +0700

    final fix

--- mytest/settings.py
+++ mytest/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 SECRET_KEY = 'nyg1guv)*q)1gr+-n%kf$ucgvc0hdqk=_9*-y-zdtbb!!ro99!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 TEMPLATE_DEBUG = False
 
